87f741e51734096ef578bc8af487e757d2a45ff6
dairinka <104695300+dairinka@users.noreply.github.com>
2022-08-18 13:53:54 +0300

    feat: change branch

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -9,40 +9,40 @@ export default class AudioPlayer {
         this.audioContext = null;
     }
 
-    createVisualiser() {
-        this.audioContext = new AudioContext();
-        this.src = this.audioContext.createMediaElementSource(this.audioElem);
-        const analyser = this.audioContext.createAnalyser();
-        const canvas = this.visualiserElem;
-        const ctx = canvas.getContext('2d');
-        this.src.connect(analyser);
-        analyser.connect(this.audioContext.destination);
-        analyser.fftSize = 128;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-        const barWidth = (canvas.width / bufferLength) * 2.5;
-        let barHeight;
-        let bar;
+    // createVisualiser() {
+    //     this.audioContext = new AudioContext();
+    //     this.src = this.audioContext.createMediaElementSource(this.audioElem);
+    //     const analyser = this.audioContext.createAnalyser();
+    //     const canvas = this.visualiserElem;
+    //     const ctx = canvas.getContext('2d');
+    //     this.src.connect(analyser);
+    //     analyser.connect(this.audioContext.destination);
+    //     analyser.fftSize = 128;
+    //     const bufferLength = analyser.frequencyBinCount;
+    //     const dataArray = new Uint8Array(bufferLength);
+    //     const barWidth = (canvas.width / bufferLength) * 2.5;
+    //     let barHeight;
+    //     let bar;
         
-        function renderFrame() {
-            requestAnimationFrame(renderFrame);
-            bar = 0;
-            analyser.getByteFrequencyData(dataArray);
+    //     function renderFrame() {
+    //         requestAnimationFrame(renderFrame);
+    //         bar = 0;
+    //         analyser.getByteFrequencyData(dataArray);
       
-            ctx.fillStyle = "#000";
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //         ctx.fillStyle = "#000";
+    //         ctx.fillRect(0, 0, canvas.width, canvas.height);
       
-            for (let i = 0; i < bufferLength; i++) {
-              barHeight = dataArray[i] - 75;
-              const r = barHeight + (25 * (i/bufferLength));
-              ctx.fillStyle = `rgb(${r}, 100, 50)`;
-              ctx.fillRect(bar, canvas.height - barHeight, barWidth, barHeight);
-              bar += barWidth + 2;
-            }
-          }
+    //         for (let i = 0; i < bufferLength; i++) {
+    //           barHeight = dataArray[i] - 75;
+    //           const r = barHeight + (25 * (i/bufferLength));
+    //           ctx.fillStyle = `rgb(${r}, 100, 50)`;
+    //           ctx.fillRect(bar, canvas.height - barHeight, barWidth, barHeight);
+    //           bar += barWidth + 2;
+    //         }
+    //       }
 
-          renderFrame();
-    }
+    //       renderFrame();
+    // }
 
     createPlayerElements() {
         this.audioElem = document.createElement('audio');
